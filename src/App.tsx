013c48882d3fabbfdd475fc18935e92d858ed761
Egoitz <egoitz@fisify.com>
2026-04-09 17:59:29 +0200

    fix: redirect prudential aig routes to home

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
 import ClientPresentationApp from './components/ClientPresentationApp'
 import PrudentialApp from './pages/PrudentialApp'
-import AIGApp from './pages/AIGApp'
 import ZurichApp from './pages/ZurichApp'
 
 function App() {
@@ -10,8 +9,8 @@ function App() {
       <Routes>
         <Route path="/" element={<PrudentialApp />} />
         <Route path="/prudential" element={<PrudentialApp />} />
-        <Route path="/AIG" element={<AIGApp homePath="/AIG" />} />
-        <Route path="/aig" element={<AIGApp homePath="/AIG" />} />
+        <Route path="/AIG" element={<Navigate to="/" replace />} />
+        <Route path="/aig" element={<Navigate to="/" replace />} />
         <Route path="/Zurich" element={<ZurichApp homePath="/Zurich" />} />
         <Route path="/zurich" element={<ZurichApp homePath="/zurich" />} />
         <Route path="/medicus" element={<ClientPresentationApp clientName="Medicus" clientLogo="/images/medicus-logo-white.png" homePath="/medicus" />} />
